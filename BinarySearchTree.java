053b56469a3297533be4fca03c2a29bdb4b5c092
Brandon Alfaro <brandonlfr@MacBook-Air.attlocal.net>
2023-10-21 15:43:44 -0500

    Added a working add() find() and set_find() function

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -15,6 +15,8 @@ import java.util.Scanner;
 // Adding my own imports
 import java.util.Stack;
 
+import javax.swing.RowFilter.Entry;
+
 
 // Understanding this Syntax
 // "BinarySearchTree<T extends Comparable<? super T>> implements Iterable<T>"
@@ -55,53 +57,128 @@ public class BinarySearchTree<T extends Comparable<? super T>> implements Iterab
         return false;
     }
 
-    // add() function that adds elements
+    // "add()" function adds elements
     public boolean add(T x) {
+
+        // Set boolean var to return success of adding new node
         boolean add_success = false;
 
-        // If root is null, add the node to root and have size + 1 and return true
+        // If root is null go here and assign the new node to root.
         if (root == null){
+
+            // Allocate new node and insert it to root
             Entry<T> new_root = new Entry<>(x, null, null);
             root = new_root;
 
+            // Increase size by 1 and return true
             size++;
             add_success = true;
         }
+        // If root is not null then find where to place new node
         else {
-            Entry<T> element_extracted = find(root, x);
-            System.out.println(element_extracted);
+
+            // Find if x exist in tree or find the lowest point where
+            //  we can place new node either left or right of parent. 
+            Entry<T> element_extracted = set_find(x);
+
+            // If x does not exist in tree, place 
+            // either left or right of parent node
+            if (x.compareTo(element_extracted.element) != 0){
+                
+                // Place left of parent
+                if (x.compareTo(element_extracted.element) < 0){
+                    element_extracted.left = new Entry<>(x, null, null);
+                }
+                // Place right of parent
+                else {
+                    element_extracted.right = new Entry<>(x, null, null);
+                }
+                // Increase size + 1
+                size++;
+                add_success = true;
+            }
         }
-
-
-
+        // Reset Stack and return success of adding node, true or false.
+        binary_stack = null;
         return add_success;
     }
 
     ////////////////////////////////
+
+    
     // remove() function that removes elements
-    public T remove(T x) {
+    public T remove(T x){
         return null;
     }
 
 
     // Helper function for remove and add
         
+
+    private Entry<T> set_find(T x){
+
+        binary_stack = new Stack<>();
+        binary_stack.add(null);
+        Entry<T> return_node = find(root, x);
+
+        return return_node;
+    }
+
     // find() Function finds element if it exist or not.
     private Entry<T> find(Entry<T> root, T x){
 
         // Create a null Entry, return back Entry in tree if found
-        Entry<T> return_entry = null;
 
-        if (root == null || root.element == x){
-            return_entry = root;
+        Entry<T> current_entry = root;
+        boolean found_element = false;
+
+
+        // If we root is null or root equals
+        // to new element, return back the node.
+        if (current_entry == null || current_entry.element == x){
+            found_element = true;
         }
 
+        // Create boolean loop to stop while()
+        boolean stop_loop = false;    
+        
+        // Loop until we find leaf of a perfect parent or
+        // if there already exists a x in the tree.
+        while(found_element == false && stop_loop == false){
 
-        binary_stack = new Stack<>();
-        binary_stack.push(null);
+            // Go here if x < current.element
+            if(x.compareTo(current_entry.element) < 0){
 
-
-        return return_entry;
+                // If left is null, stop while() and 
+                // return current parent node
+                if (current_entry.left == null){
+                    stop_loop = true;
+                }
+                // If left is not null, add "current_entry"
+                // to stack and assign it to its own left child 
+                else{
+                    binary_stack.add(current_entry);
+                    current_entry = current_entry.left;
+                }
+            }
+            // If x exist, break loop and return "current_entry"
+            else if(x.compareTo(current_entry.element) == 0 ){
+                stop_loop = true;
+            }
+            // if "current_entry" is null, stop while()
+            // and return current parent node
+            else if(current_entry.right == null){
+                stop_loop = true;
+            }
+            // If not null assign "current_entry"
+            //  to its own right child
+            else{
+                binary_stack.add(current_entry);
+                current_entry = current_entry.right;
+            }
+        }
+        // Return Node 
+        return current_entry;
     }
     // splice()
 
@@ -199,7 +276,6 @@ public class BinarySearchTree<T extends Comparable<? super T>> implements Iterab
                     operand = sc.nextInt();
 
                     System.out.println("ADDING");
-                    
                     if (bst.add(operand)) {
                         result = (result + 1) % modValue;
                     }
@@ -207,13 +283,14 @@ public class BinarySearchTree<T extends Comparable<? super T>> implements Iterab
                     break;
                 }
                 case "Remove": {
-                    
+
+                    System.out.println("REMOVE");
                     operand = sc.nextInt();
-                    /*
+                    
                     if (bst.remove(operand) != null) {
                         result = (result + 1) % modValue;
                     }
-                    */
+                    
                     break;
                 }
                 case "Contains": {
